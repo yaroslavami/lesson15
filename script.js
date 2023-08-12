3b28f08604f9a1ccf4e1bd551e970f03d6dee022
Yaroslava Mishchenko <yaroslavamishsce@gmail.com>
2023-08-11 21:50:53 -0400

    update

---
+++ script.js
@@ -0,0 +1,16 @@
+//1.  1. відкрити в браузері вікно для вводу даних
+//2. ввести дані та підтвердити
+//3. зберегти дані в змінну
+//4. вивести текст зі змінної на екран
+
+//2.  1. відкрити в браузері вікно для вводу даних
+//2. ввести дані та підтвердити
+//3. зберегти дані в змінну
+//4. викликаємо вспливаюче вікно з текстом 'hello _name from var_'
+
+var n = prompt('Your name');
+console.log('Name', n);
+var i = prompt('Your Nickname', i);
+console.log('Nickname', i);
+alert('HEY!!Beaware you are not alone now...');
+document.write('Your username is ',i,n)
